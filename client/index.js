1020e1d95bcd6f83a36cbc2bb9ef35f3c60505f8
Mouhamadou <mouhamadoul.sarr@gmail.com>
2024-03-14 10:02:11 +0100

    resolving the gift list challenge

--- client/index.js
+++ client/index.js
@@ -1,14 +1,20 @@
-const axios = require('axios');
-const niceList = require('../utils/niceList.json');
-const MerkleTree = require('../utils/MerkleTree');
+const axios = require("axios");
+const niceList = require("../utils/niceList.json");
+const MerkleTree = require("../utils/MerkleTree");
 
-const serverUrl = 'http://localhost:1225';
-
+const serverUrl = "http://localhost:1225";
+const leaf = "Sidney Kertzmann";
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  // TODO: how do we prove to the server we're on the nice list?
+  const merkletree = new MerkleTree(niceList);
+  const index = niceList.indexOf(leaf);
+  const merkleProof = merkletree.getProof(index);
+
+  console.log(merkleProof);
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    merkleProof,
+    leaf,
   });
 
   console.log({ gift });
